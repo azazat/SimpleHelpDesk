d9627b1fc5f14856464bc6c197beae5a5446b50e
Azat <minm@inbox.ru>
2015-01-22 10:22:46 +0600

    symfony_starting:homepage

---
+++ authentication/app/cache/prod/twig/ea/da/6a2235d8a1370ea6f4ac08451a4b15f316a38b25fb963a7c467ade58f5f1.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/index.html.twig */
+class __TwigTemplate_eada6a2235d8a1370ea6f4ac08451a4b15f316a38b25fb963a7c467ade58f5f1 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Homepage.
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
